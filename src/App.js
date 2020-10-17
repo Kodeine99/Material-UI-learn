2046a9c7cbe1b49ab1950588ff2dcfbaabbc3a12
Kodeine99 <tung.nv2510@gmail.com>
2020-10-17 22:57:08 +0700

    modify ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header'
+import Header from './components/NavBar/Header'
 
 import 'fontsource-roboto';
 // import Typography from '@material-ui/core/Typography';
